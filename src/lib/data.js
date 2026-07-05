e25425e35e240e4c9fb26b95aa80ee0a943ef4e1
Zhuanz密码0000 <Zhuanz@MacBook-Air.local>
2026-07-05 20:24:39 +1200

    feat: Set Mina status to available

--- src/lib/data.js
+++ src/lib/data.js
@@ -59,7 +59,7 @@ export const girls = [
     ],},
   {id: 58,
     name: { en: "Mina", cn: "Mina" },
-    status: "leave",
+    status: "available",
     verified: true,
     location: "CBD",
     stats: {
